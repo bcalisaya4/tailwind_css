72b9d5c242d2d27117e25372922fd7ade44862a0
bcalisaya4 <u17200830@utp.edu.pe>
2021-10-14 09:32:20 -0500

    first commit tailwind

---
+++ tailwind.conf.js
@@ -0,0 +1,17 @@
+module.exports = {
+    purge:[
+        './public/**/*.html',
+    ],
+    darkMode: false,
+    theme:{
+        extend:{
+            backgroundImage: theme => ({
+                'wave-pattern': 'url(../img/wave-white.png)'
+            })
+        }
+    },
+    variants:{
+        extend:{},
+    },
+    plugins:[],
+}
